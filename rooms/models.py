215575db9abc69017ec6bc7930215978245ff960
assam <67585123+assam06@users.noreply.github.com>
2020-09-12 01:46:32 +0900

    ~4.3 add Room models

--- rooms/models.py
+++ rooms/models.py
@@ -6,6 +6,25 @@ from core import models as core_models
 from users import models as user_models
 
 
+class AbstractItem(core_models.TimeStampedModel):
+    # 카테고리만 다를 뿐 카테고리마다 전부 item이들어가서 만든거
+    """ Abstract Item """
+
+    # 1. 이름넣을 칸을 만듦
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+# 1-1. 그 이름이 여기에 들어감
+class RoomType(AbstractItem):
+    pass
+
+
 class Room(core_models.TimeStampedModel):
 
     """ Room Model Definition """
@@ -21,7 +40,12 @@ class Room(core_models.TimeStampedModel):
     bedrooms = models.IntegerField()
     baths = models.IntegerField()
     check_in = models.TimeField()
-    check_out = models.TextField()
+    check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
     # Foreign Key는 연결이 필요해 - room이랑 user랑 연결함
+    room_type = models.ManyToManyField(RoomType, blank=True)
+
+    # 이걸 써줘야 등록한 이름이 나옴....흠
+    def __str__(self):
+        return self.name
