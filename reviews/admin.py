318cea0ad39e7fed6fcaacece03c1736abb5cea2
assam <67585123+assam06@users.noreply.github.com>
2020-09-13 00:20:53 +0900

    5.0 review model & django's relationship

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,11 @@
+# admin은 모델을 만든 후 설치하는 데임
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Review)
+class ReviewAdmin(admin.ModelAdmin):
+
+    """ Review Admin Definition """
+
+    pass
--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from core import models as core_models
 
-# Create your models here.
+
+class Review(core_models.TimeStampedModel):
+
+    """ Review Model Definition """
+
+    review = models.TextField()
+    accuracy = models.IntegerField()
+    communication = models.IntegerField()
+    cleanliness = models.IntegerField()
+    location = models.IntegerField()
+    check_in = models.IntegerField()
+    check_out = models.IntegerField()
+    value = models.IntegerField()
+    # 유저를 삭제하면 모델(리뷰)도 사라져야하지
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    # 그리고 리뷰는 객실이랑 연결되어 있지
+    room = models.ForeignKey("rooms.Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.review} - {self.room}"  # self.room 자체가 str을 불러서 name을 안부른것.
+        # room이라고 써도 장고는 foreignKey를 보고 접근을 허용할거야.
+        # self.room.country라고 하면 그대로 출력.
+        # 이게 다 foreign key를 정의해서 그럼.
