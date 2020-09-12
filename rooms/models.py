b1936e7d61b7b99ef6ce2ce9063913c561369002
assam <67585123+assam06@users.noreply.github.com>
2020-09-12 23:47:21 +0900

    Meta class and Photo Model

--- rooms/models.py
+++ rooms/models.py
@@ -22,7 +22,50 @@ class AbstractItem(core_models.TimeStampedModel):
 
 # 1-1. 그 이름이 여기에 들어감
 class RoomType(AbstractItem):
-    pass
+
+    """ RoomType Model Definition"""
+
+    class Meta:
+        verbose_name = "Room Type"
+
+
+class Amenity(AbstractItem):
+
+    """ Amenity Model Definition """
+
+    # 이름 변경하는 코드
+    class Meta:
+        verbose_name_plural = "Amenities"
+
+
+class Facility(AbstractItem):
+
+    """ Facility Model Definition """
+
+    class Meta:
+        verbose_name_plural = "Facilities"
+
+
+class HouseRule(AbstractItem):
+
+    """ HouseRule Model Definition """
+
+    class Meta:
+        verbose_name = "House Rule"
+
+
+class Photo(core_models.TimeStampedModel):
+
+    """ Photo Model Definition """
+
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+
+    # 파이썬은 파일을 상하수직방향으로 읽음. ""(string)을 하지 않으면 장고는 Room(모델)을 정의 못해.
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+
+    def ___str___(self):
+        return self.caption
 
 
 class Room(core_models.TimeStampedModel):
@@ -42,10 +85,16 @@ class Room(core_models.TimeStampedModel):
     check_in = models.TimeField()
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
-    host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
-    # Foreign Key는 연결이 필요해 - room이랑 user랑 연결함
-    room_type = models.ManyToManyField(RoomType, blank=True)
+    host = models.ForeignKey("users.User", on_delete=models.CASCADE)
+
+    # 방의 타입은 roomtype중 한 종류여야해. 여러객실 유형이어선 안돼.
+    # 객실 유형은 한가지 또는 다른유형 그리고 삭제하는 경우엔 Room을 삭제해선 안돼.
+    room_type = models.ForeignKey("RoomType", on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField("Amenity", blank=True)
+    facilities = models.ManyToManyField("Facility", blank=True)
+    house_rules = models.ManyToManyField("HouseRule", blank=True)
 
     # 이걸 써줘야 등록한 이름이 나옴....흠
+    # 생성된 obj or model을 지칭하는 것을 뭘로 설정할거야? 의 의미
     def __str__(self):
         return self.name
