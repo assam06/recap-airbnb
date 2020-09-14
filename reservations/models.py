15f699c0e3787955c6c68a1d6c69b63f46afb5b8
assam <67585123+assam06@users.noreply.github.com>
2020-09-14 21:02:47 +0900

    Reservations,List model

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from core import models as core_models
 
-# Create your models here.
+
+class Reservation(core_models.TimeStampedModel):
+
+    """ Reservation Model Definition """
+
+    # 예약 상태 넣을거야
+
+    STATUS_PENDING = "pending"
+    STATUS_CONFIRMED = "confirmed"
+    STATUS_CANCELED = "canceled"
+
+    STATUS_CHOICES = (
+        (STATUS_PENDING, "Pending"),
+        (STATUS_PENDING, "Confirmed"),
+        (STATUS_PENDING, "Canceled"),
+    )
+
+    status = models.CharField(
+        max_length=12, choices=STATUS_CHOICES, default=STATUS_PENDING
+    )
+    check_in = models.DateField()
+    check_out = models.DateField()
+    guest = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    room = models.ForeignKey("rooms.Room", on_delete=models.CASCADE)
+
+    # 아... self가 reservation이라고 나오네? class 내의 self라서 그런가봐!
+    def __str__(self):
+        return f"{self.room} - {self.check_in}"
