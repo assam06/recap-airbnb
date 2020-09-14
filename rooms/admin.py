40b6c21d9a2a2b6d9448f5b77b5e990ceb302a1f
assam <67585123+assam06@users.noreply.github.com>
2020-09-15 00:30:12 +0900

    Room admin panel part Two

--- rooms/admin.py
+++ rooms/admin.py
@@ -12,12 +12,63 @@ class ItemAdmin(admin.ModelAdmin):
     pass
 
 
+# 모델이 외부에서 보여지는 방식(리스트처럼) 과 안에서 보이는 방식을 변경해
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
 
     """ Room Admin Definition """
 
-    pass
+    fieldsets = (
+        ("Basic Info", {"fields": ("name", "description", "country", "city", "price")}),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
+        (
+            "More About the Space",
+            {  # 접는 섹션
+                "classes": ("collapse",),
+                "fields": ("amenities", "facilities", "house_rules"),
+            },
+        ),
+        (
+            "Spaces",
+            {"fields": ("guests", "beds", "bedrooms", "baths")},
+        ),
+        ("Last Details", {"fields": ("host",)}),
+    )
+
+    list_display = (
+        "name",
+        "description",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+    )
+
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "city",
+        "country",
+    )
+
+    search_fields = ("=city", "^host__username")
+
+    # 기본적으로  ManytoMany로 작동함. 장고의 룰
+    filter_horizontal = (
+        "amenities",
+        "facilities",
+        "house_rules",
+    )
 
 
 @admin.register(models.Photo)
